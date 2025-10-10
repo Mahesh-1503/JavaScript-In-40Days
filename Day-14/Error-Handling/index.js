1e7b9788434d8adf310ab1c427912a2a8c30b254
Mahesh-1503 <mnopxyz090@gmail.com>
2025-10-10 17:56:46 +0530

    Update index.js

--- Day-14/Error-Handling/index.js
+++ Day-14/Error-Handling/index.js
@@ -183,13 +183,13 @@ try {
     console.error(`${error.name}: ${error.message}`)
 }
 
-// Self Assignment Operator ?=
+// Self Assignment Operator ??=
 
 let x;
 let y = 10;
 
-x ?= 20; // x is undefined, so x becomes 20
-y ?= 30; // y is already 10, so y remains 10
+x ??= 20; // x is undefined, so x becomes 20
+y ??= 30; // y is already 10, so y remains 10
 
 console.log(x); // Output: 20
 console.log(y); // Output: 10
