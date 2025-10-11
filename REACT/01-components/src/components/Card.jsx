f6c09ee6f43d8658595d67921e8a2288fa62a0a2
Mahesh-1503 <mnopxyz090@gmail.com>
2025-10-12 03:00:25 +0530

    *

---
+++ REACT/01-components/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  const name = "Mahesh Kumar";
+  return (
+    <>
+      <div className="card">
+        <h1>{name}</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Card;
